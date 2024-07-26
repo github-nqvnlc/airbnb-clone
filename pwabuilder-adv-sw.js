4bc36096df8185d20c2ad79a93de550bbd3d1981
louis-jirasr <locnv14@gmail.com>
2024-07-26 16:09:46 +0700

    setup PWA web

--- pwabuilder-adv-sw.js
+++ pwabuilder-adv-sw.js
@@ -1,3 +1,7 @@
-
-  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-  precacheAndRoute([{"revision":"60b0d208d4536361f659f78fee5ff6ff","url":"app/globals.css"},{"revision":"4d0b201a9f65432e3981063eb9d61d88","url":"next.config.js"},{"revision":"854b38759e7a8b4b82306ae2d9a3a833","url":"postcss.config.js"},{"revision":"7d82bc8ae15de19ebfeda42d66d6203b","url":"tailwind.config.js"}]);
+import { precacheAndRoute } from "workbox-precaching/precacheAndRoute";
+precacheAndRoute([
+  { revision: "60b0d208d4536361f659f78fee5ff6ff", url: "app/globals.css" },
+  { revision: "4d0b201a9f65432e3981063eb9d61d88", url: "next.config.js" },
+  { revision: "854b38759e7a8b4b82306ae2d9a3a833", url: "postcss.config.js" },
+  { revision: "7d82bc8ae15de19ebfeda42d66d6203b", url: "tailwind.config.js" },
+]);
